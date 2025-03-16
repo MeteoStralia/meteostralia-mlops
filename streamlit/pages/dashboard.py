010447ddf1aa4e2487e26b6420f874b6bd58e10a
mathieu amacher <amacher_mathieu@hotmail.com>
2025-03-16 18:59:20 +0100

    minor fix

--- streamlit/pages/dashboard.py
+++ streamlit/pages/dashboard.py
@@ -9,13 +9,16 @@ st.write('page_dashboard from streamlit')
 st.write('############ from api')
 
 token = st.session_state.get("token", None)
+headers = {'Authorization' :f'Bearer {token}'}
+response = requests.get('http://localhost:1111/dashboard', headers = headers )
 
 
 if token:
-    headers = {'Authorization' :f'Bearer {token}'}
 
-    response = requests.get('http://localhost:1111/dashboard', headers = headers )
     st.write(response.status_code)
     st.write(response.json())
     # st.write('username :', response.json()['username'])
     # st.write('scope :', response.json()['scope'])
+
+else:
+    st.write(response.status_code)
--- streamlit/pages/me.py
+++ streamlit/pages/me.py
@@ -5,13 +5,15 @@ import requests
 st.write('page me from streamlit')
 
 token = st.session_state.get("token", None)
+headers = {'Authorization' :f'Bearer {token}'}
+response = requests.get('http://localhost:1111/users/me', headers = headers )
+
 
 if token:
-    headers = {'Authorization' :f'Bearer {token}'}
-
-    response = requests.get('http://localhost:1111/users/me', headers = headers )
     st.write(response.status_code)
 
     st.write('################### from api')
     st.write('name :', response.json()['name'])
     st.write('scope :', response.json()['scope'])
+else:
+    st.write(response.status_code)
