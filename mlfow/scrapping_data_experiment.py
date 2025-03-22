58c18d5d91a94f220d69bb55fb369833177b94f7
alastirm <bruno.vermont@gmail.com>
2025-03-22 13:30:49 +0100

    Update scrapping_data_experiment.py

--- mlfow/scrapping_data_experiment.py
+++ mlfow/scrapping_data_experiment.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import sys
 sys.path.append('../src/')
+sys.path.append('../')
 
 from src.data_service.ingest_data.scrap_data import scrap_data, format_scrapped_data, save_new_data
 from src.data_service.ingest_data.ingest_new_data import reindex_data
@@ -8,8 +9,11 @@ from src.data_service.ingest_data.ingest_new_data import reindex_data
 new_data_folder = "../data/new_data/"
 # set year and month of data
 
+years_scrap = ["2024","2025"]
 months_scrap = {"2024": ["02","05","08","11"],
                 "2025": ["01"]}
+
+
 # Locations to request
 location_list = ["Canberra","Sydney","Melbourne","Brisbane"]
 station_ID = pd.read_csv("../data/add_data/station_ID.csv", sep=";")
@@ -23,16 +27,21 @@ id_list = station_ID.loc[station_ID["Location"].isin(location_list)]
 scrapped_data = pd.DataFrame()
 # scrap data for all locations
 
-for location_name in id_list:
-        year=year_scrap,
-        month=month_scrap)
-    lastdata_location_c = lastdata_location.copy()
-    # formatting as raw data
-    lastdata_location_c = format_scrapped_data(lastdata_location_c)
-    lastdata_location_c = reindex_data(lastdata_location_c)
-    # concatenate with all stations data
-    scrapped_data = pd.concat([scrapped_data, 
-                                        lastdata_location_c])
+for year_scrap in years_scrap:
+    for month_scrap in months_scrap[year_scrap]:
+        for location_name in location_list:
+
+            id_location = id_list.loc[id_list.Location == location_name,"IDCJDW"]
+            id_location = str(id_location.iloc[0])
+            data_location = scrap_data(
+                location_name=location_name,
+                id_location=id_location,
+                year=year_scrap,
+                month=month_scrap)
+            data_location_c = data_location.copy()
+            # formatting as raw data
+            data_location_c = format_scrapped_data(data_location_c)
+            data_location_c = reindex_data(data_location_c)
             # concatenate with all stations data
             scrapped_data = pd.concat([scrapped_data, 
                                                 data_location_c])
@@ -40,11 +49,10 @@ for location_name in id_list:
 # copy scrapped data
 scrapped_data_c = scrapped_data.copy()
 
-# save scrapped data to new_data path to be added in training data (except data for predict_date)
-save_data = scrapped_data_c.loc[
-    scrapped_data_c["Date"].dt.strftime('%Y-%m-%d').isin([predict_date, predict_date_yesterday]) == False]
+save_data = scrapped_data_c
 
 save_new_data(save_data, new_data_folder)
+
 
 
 # # testing 
