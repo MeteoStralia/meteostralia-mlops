7442e75c8605f338d11b1deaca6bb58189bd5d84
Bruno Vermont <bruno.vermont@gmail.com>
2025-03-29 16:11:06 +0100

    dvc changes

--- src/tracking_service/dvc/dvc_push.py
+++ src/tracking_service/dvc/dvc_push.py
@@ -9,7 +9,7 @@ with open("metrics/RainTomorrow/lastmetrics.json", "r") as file:
 
 tag = list(metrics.keys())[0]
 subprocess.run('dvc commit -q')
-subprocess.run('git add DVC.lock')
+subprocess.run('git add dvc.lock')
 subprocess.run(f"git commit -m 'datamodel'")
 subprocess.run(f"git tag -a {tag} -m 'datamodel'")
 subprocess.run('git push origin --tags')
